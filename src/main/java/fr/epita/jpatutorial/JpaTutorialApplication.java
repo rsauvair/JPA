ebecf80d18c9c0c03cd5f11918aec8a6573aefab
Romain Sauvaire--Dassac <romain.sauvaire-dassac@epita.fr>
2018-10-27 00:16:37 +0200

    class_moto.png updated

--- src/main/java/fr/epita/jpatutorial/JpaTutorialApplication.java
+++ src/main/java/fr/epita/jpatutorial/JpaTutorialApplication.java
@@ -1,6 +1,5 @@
 package fr.epita.jpatutorial;
 
-import fr.epita.jpatutorial.entities.Moto;
 import fr.epita.jpatutorial.repositories.MotoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,12 +22,7 @@ public class JpaTutorialApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new Moto("Yamaha", "Fazer", 600));
-        repository.save(new Moto("Yamaha", "R1", 1000));
-        repository.save(new Moto("Kawasaki", "Z750", 750));
-        repository.findByBrand("Yamaha").forEach(moto ->
-                logger.warn(moto.toString())
-        );
+        // TODO : do some stuff here
     }
 }
 
